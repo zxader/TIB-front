9d8c5058c5e9eaeecb94bda41ed4d63b09b50e09
zxader <zxader@naver.com>
2025-12-16 23:43:51 +0900

    fix: 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { i18nReplace } from "./plugins/vite-plugin-i18n-replace";
 
+const isProd = process.env.NODE_ENV === "production";
 const lang = process.env.VITE_I18N_LANG || "ko";
 
 export default defineConfig({
   plugins: [react(), i18nReplace(lang)],
-  base: `/${lang}/`,
+  base: isProd ? `/${lang}/` : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
