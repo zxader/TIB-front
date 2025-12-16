0916419fbb386297745685020d452c836774871e
zxader <zxader@naver.com>
2025-12-16 23:10:10 +0900

    fix: config수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,14 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { i18nReplace } from "./plugins/vite-plugin-i18n-replace";
 
+const lang = process.env.VITE_I18N_LANG || "ko";
+
 export default defineConfig({
-  plugins: [react(), i18nReplace(process.env.VITE_I18N_LANG || "ko")],
+  plugins: [react(), i18nReplace(lang)],
+  base: `/${lang}/`,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  define: {
-    "process.env.VITE_I18N_LANG": JSON.stringify(
-      process.env.VITE_I18N_LANG || "ko"
-    ),
   },
 });
